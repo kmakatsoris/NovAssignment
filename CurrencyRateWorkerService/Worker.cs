d02e1dc4b14c112eab941ad2a8b9c6c9f2537c4e
maccosGit <3thirdDimension3D@gmail.com>
2024-10-06 01:56:19 +0300

    Remove Obj and Bins

--- CurrencyRateWorkerService/Worker.cs
+++ CurrencyRateWorkerService/Worker.cs
@@ -1,23 +1,39 @@
-namespace CurrencyRateWorkerService;
+using Microsoft.Extensions.Configuration;
 
+namespace CurrencyRateWorkerService;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly EcbGateway _ecbGateway;
+    private readonly int _fetchInterval;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _ecbGateway = new EcbGateway();
+        _fetchInterval = configuration.GetValue<int>("Worker:FetchIntervalMinutes");
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+            try
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                List<CurrencyRate> rates = await _ecbGateway.GetCurrencyRatesAsync();
+                foreach (var rate in rates)
+                {
+                    _logger.LogInformation($"Currency: {rate.Currency}, Rate: {rate.Rate}, Date: {rate.Date}");
+                }
             }
-            await Task.Delay(1000, stoppingToken);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error fetching currency rates");
+            }
+
+            await Task.Delay(TimeSpan.FromMinutes(_fetchInterval), stoppingToken);
         }
     }
 }
